19efd526551dab6d0b438c81d7e267b532dada06
Fernando Laube <fernando.laube@gmail.com>
2020-10-24 15:54:09 -0300

    Adds minor changes and final curve function

--- nozzle.py
+++ nozzle.py
@@ -12,8 +12,7 @@ Y_0 [-]: Raio na garganta do bocal, normalizado pelo comprimento do bocal
 
 GAMMA = 1.4
 M_TEST = 4
-Y_0 = 5.07
-Y_1 = 10.14
+Y_0 = 10
 
 
 def main():
@@ -21,34 +20,43 @@ def main():
 
     A_test_ratio = A_ratio(M_TEST)
 
-    theta_1_max_rad = v_test_rad/2
-    theta_1_max_deg = v_test_deg/2
+    # theta_1_max_rad = v_test_rad/2
+    # theta_1_max_deg = v_test_deg/2
 
     theta_1_rad, theta_1_deg = empirical_theta_1(v_test_rad, A_test_ratio)
 
-    print(theta_1_max_deg)
-    print(theta_1_deg)
+    v_1_rad = v_test_rad - theta_1_rad
+    v_1_deg = v_test_deg - theta_1_deg
 
-    x_initial_curve, y_initial_curve = initial_curve(Y_0, Y_1, theta_1_rad)
-    y_initial_curve_mirror = -y_initial_curve
-    separating_dashed_line = np.zeros(x_initial_curve.shape)
+    M_1 = M_from_v(v_1_rad)
+
+    x_final_curve, y_final_curve = foelsch(Y_0, theta_1_rad, v_1_rad,
+                                           v_test_rad, M_1)
+
+    y_1 = y_final_curve[0]
+
+    x_initial_curve, y_initial_curve = initial_curve(Y_0, y_1, theta_1_rad)
 
     fig, ax = plt.subplots()
     ax.plot(x_initial_curve, y_initial_curve, 'r-')
-    ax.plot(x_initial_curve, y_initial_curve_mirror, 'r-')
-    ax.plot(x_initial_curve, separating_dashed_line, 'k--')
+    ax.plot(x_initial_curve, -y_initial_curve, 'r-')
+    ax.plot(x_final_curve, y_final_curve, 'r-')
+    ax.plot(x_final_curve, -y_final_curve, 'r-')
+    ax.plot(x_initial_curve, np.zeros(x_initial_curve.shape), 'k--')
+    ax.plot(x_final_curve, np.zeros(x_final_curve.shape), 'k--')
     ax.set_title("")
     ax.set_xlabel("")
     ax.set_ylabel("")
 
     # plt.grid()
-    plt.xlim(-25, 100)
-    plt.xticks(np.arange(-25, 101, 5))
-    plt.yticks(np.arange(-30, 31, 5))
+    plt.xlim(x_initial_curve[0], x_final_curve[-1]*1.05)
+    plt.ylim(-y_final_curve[-1]*2, y_final_curve[-1]*2)
+    # plt.xticks(np.arange(x_initial_curve[0], x_final_curve[-1]*1.05, 5))
+    # plt.yticks(np.arange(-y_final_curve[-1]*1.25, y_final_curve[-1]*1.25, 5))
     plt.show()
 
 
-def empirical_theta_1(v_t, A_ratio=0, A_star=0, A_test=0):
+def empirical_theta_1(v_t, A_test_ratio=0, A_star=0, A_test=0):
     """
     Retorna o valor do parâmetro theta_1,
     calculado por meio de uma relação empírica,
@@ -56,13 +64,14 @@ def empirical_theta_1(v_t, A_ratio=0, A_star=0, A_test=0):
     a área na seção de teste (A_test) e o ângulo
     de expansão na seção de teste (v_t).
 
-    A_ratio espera a relação A/A*
+    A_test_ratio - relação A/A*.
+    v_t - [rad]
     """
 
-    if A_ratio == 0:
-        A_ratio = A_test/A_star
+    if A_test_ratio == 0:
+        A_test_ratio = A_test/A_star
 
-    theta_1 = (1/A_ratio) ** (2/9) * (v_t/2)
+    theta_1 = ((1/A_test_ratio) ** (2/9)) * (v_t/2)
 
     return (theta_1, math.degrees(theta_1))
 
@@ -107,10 +116,89 @@ def initial_curve(y_0, y_1, theta_1):
     theta_1 [rad]
     """
     x_1 = 3/(2*math.tan(theta_1))*(y_1 - y_0)
-    x = np.arange(-25, x_1, 0.01)
+    x = np.arange(-50, x_1, 0.01)
     y = y_0 + math.tan(theta_1)/x_1*(x ** 2)*(1 - x/(3*x_1))
 
     return (x, y)
+
+
+def foelsch(y_0, theta_1, v_1, v_test, M_1):
+    """
+    Calcula os pontos da curva final (após o ponto de inflexão do bocal),
+    utilizando o Método de Foelsch.
+
+    theta_1 - [rad]
+    v_1 - [rad]
+    v_test - [rad]
+    """
+
+    r_0 = y_0/theta_1
+
+    A_1_ratio = A_ratio(M_1)
+
+    y_1 = r_0*A_1_ratio*math.sin(theta_1)
+    x_1 = 3/(2*math.tan(theta_1))*(y_1 - y_0)
+
+    r_1 = y_1/math.sin(theta_1)
+
+    M_array = np.arange(M_1, M_TEST, 0.01)
+    x_array = np.zeros(M_array.shape)
+    y_array = np.zeros(M_array.shape)
+
+    for i, M in enumerate(M_array):
+        a = math.asin(1/M)
+
+        A_ratio_local = A_ratio(M)
+        r_local = r_0*A_ratio_local
+        v_local, _ = v(M)
+        l_local = M*r_local*(v_local - v_1)
+
+        x_2 = (x_1 - r_1*math.cos(theta_1) +
+               r_local*math.cos(v_test - v_local))
+        y_2 = r_local*math.sin(v_test - v_local)
+
+        x_array[i] = x_2 + l_local*math.cos(v_test - v_local + a)
+        y_array[i] = y_2 + l_local*math.sin(v_test - v_local + a)
+
+    return (x_array, y_array)
+
+
+def M_from_v(v_target, gamma=1.4):
+    """
+    Calcula o número de Mach dado o ângulo de expansão em radianos,
+    de forma iterativa. Assume-se intervalos para o número de Mach,
+    que são atualizados e reduzidos a cada vez que há uma troca de sinal
+    na subtração entre o valor estimado e o valor 'v_target'.
+
+    v_target - [rad]
+    """
+    lower_boundary = 1
+    upper_boundary = 1000
+
+    tolerance = 1e-6
+
+    while True:
+        interval = np.linspace(lower_boundary, upper_boundary, 10)
+
+        previous_est = math.inf
+        current_est = math.inf
+
+        for i, M in enumerate(interval):
+            current_v, _ = v(M)
+
+            current_est = v_target - current_v
+
+            if abs(current_est) <= tolerance:
+                return M
+            elif i != 0:
+                # Atualiza fronteiras de busca
+                if current_est*previous_est < 0:
+                    # Regra do sanduíche (se houve mudança de sinal entre dois
+                    # pontos, o zero da função está entre esses dois pontos)
+                    lower_boundary = interval[i-1]
+                    upper_boundary = M
+                    break
+            previous_est = current_est
 
 
 if __name__ == "__main__":
